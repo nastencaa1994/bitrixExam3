12909efe1d3ad2036a7200add800401d061ce46c
abogdan <abogdan@gmcs.ru>
2025-07-23 11:59:51 +0300

    удалила лишнее

--- local/modules/exam.module/lang/ru/install/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$MESS["EXAM_MODULE_MODULE_NAME"] = "Экзамен 3";
-$MESS["EXAM_MODULE_MODULE_DESC"] = "Ответы на экзамен";
-$MESS["EXAM_MODULE_NAME"] = "Anastasiya";
-$MESS["EXAM_MODULE_URI"] = "";
